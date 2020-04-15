d73cc34138b073a856272d5ce680654786779e00
kala0kala <50321842+kala0kala@users.noreply.github.com>
2020-04-15 21:16:33 +0200

    klasy+zwierze porusza się w stronę rośliny
    Przepisano kod przy użyciu funkcji i klas. Dodano moduł pozwalający na przemieszczanie się zwierzęcia w stronę rośliny

--- sztuszne_zycie.py
+++ sztuszne_zycie.py
@@ -5,10 +5,56 @@ import pygame
 import random
 import time
 import sys
+import math
+
+class Zwierze():
+    def __init__(self):
+        #określa położenie roślonożercy
+        self.poz_x=random.randrange(1, wx)
+        self.poz_y=random.randrange(1, wy)
+        self.goat_spawn = True
+        print("poz 0:", self.poz_x,self.poz_y)
+    def ruch(self):
+        smallOffset = random.randrange(-50,50)    # potrzebne, żeby roSlinożerca się przemieszczał
+        smallOffset1 = random.randrange(-50,50)   #
+        self.poz_x1 = self.poz_x+smallOffset1    # generuje nową pozycję roślinożercy
+        self.poz_y1 = self.poz_y+smallOffset     #
+        print("next position: ", self.poz_x1,self.poz_y1)
+        if self.poz_x1 >= 480: #Zeby roślinożerca nie wyszedł poza planszę
+            self.poz_x1 = 10  # Ale chyba nie działa
+        if self.poz_y1 >= 480: #
+            self.poz_y1 = 10  #
+        if self.poz_x1 <= 10:  #
+            self.poz_x1 = 480 #
+        if self.poz_y1 <= 10:  #
+            self.poz_y1 = 480 #
+
+class Roslina():
+    def __init__(self):
+        #określa położenie roślonożercy
+        self.poz_x=random.randrange(1, wx)
+        self.poz_y=random.randrange(1, wy)
+        self.apple_spawn = True
+
+    def kolizja(self):  #jeśli zwierze zje jabłko, to ono znika i pojawia się nowe
+        self.apple_spawn = False #znika
+        if not apple_spawn:
+            self.poz_x=random.randrange(1, wx) #losuje nową pozycję
+            self.poz_y=random.randrange(1, wy) #
+            self.apple_spawn = True # pojawia się
+
+def odl(poz_x1, poz_y1, poz_x2, poz_y2): #Funkcja służąca do wyznaczenia odległości, między dwoma punktami
+    sumax = poz_x1 - poz_x2 #
+    sumay = poz_y1 - poz_y2 #
+    suma1 = sumax*sumax #
+    suma2 = sumay*sumay #
+    suma3 = suma1 +suma2 #
+    pierwiastek = math.sqrt(suma3) #
+    return pierwiastek #
 
 pygame.init()
-wx = 500
-wy = 500
+wx = 500 #szerokość okna
+wy = 500 #wysokość okna
 
 GREEN = [0, 255, 127] #kolor tła
 RED = [255, 0, 0]    # kolor jabłka
@@ -19,51 +65,33 @@ background = pygame.Surface((wx, wy))      # w sumie to nie wiem co robi i czy b
 clock = pygame.time.Clock()
 pygame.display.set_caption('Game of life') #nazwa okna
 
-apple_pos = [random.randrange(1, (wx//10)) * 10, random.randrange(1, (wy//10)) * 10] #określa położenie jabłka
-#pygame.draw.rect(screen, RED, pygame.Rect(apple_pos[0], apple_pos[1], 10, 10)) #
-apple_spawn = True #
-
-goat_pos = [random.randrange(1, (wx//10)) * 10, random.randrange(1, (wy//10)) * 10] #określa położenie roślonożercy
-apple_pos[0] = goat_pos[0] #pozycja jabłka jest taka sama jak wyjściowa pozycja kozy,
-apple_pos[1] = goat_pos[1] # zostało to dodane do celów czysto testowych, aby sprawdzić, czy działa jedzenie jabłka przez kozę, później prawdopodobnie to usunę
-goat_spawn = True
-
 #pygame.display.flip();
-
-
+koza=Zwierze()
+apple = Roslina()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #wyjście z gry
             pygame.quit()              #
             sys.exit()                  #
 
-    smallOffset = random.randrange(0,50)    # potrzebne, żeby roSlinożerca się przemieszczał
-    smallOffset1 = random.randrange(0,50)   #
-    pos_x = goat_pos[0]+smallOffset1    # generuje nową pozycję roślinożercy
-    pos_y = goat_pos[1]+smallOffset     #
-
-    if pos_x == 480: #Zeby roślinożerca nie wyszedł poza planszę
-        pos_x == 10  # Ale chyba nie działa
-    if pos_x == 480: #
-        pos_x == 10  #
-    if pos_x == 10:  #
-        pos_x == 480 #
-    if pos_x == 10:  #
-        pos_x == 480 #
-
-    # określa ponowne położenie roślonożercy
-    if not apple_spawn: #
-        apple_pos = [random.randrange(1, (wx//10)) * 10, random.randrange(1, (wy//10)) * 10] #
-    apple_spawn = True #
-
     screen.fill(GREEN) #maluje tło
-    apple = pygame.draw.rect(screen, RED, pygame.Rect(apple_pos[0], apple_pos[1], 10, 10)) # generuje jabłko
-    goat = pygame.draw.rect(screen, BLACK, pygame.Rect(pos_x, pos_y, 10, 10)) #generuje roślinożercę
+    apple1 = pygame.draw.rect(screen, RED, pygame.Rect(apple.poz_x, apple.poz_y, 10, 10)) # generuje jabłko
+    goat = pygame.draw.rect(screen, BLACK, pygame.Rect(koza.poz_x, koza.poz_y, 10, 10)) #generuje roślinożercę
 
-    if apple.colliderect(goat): #zjadanie roślin przez kozy
-        apple_spawn = False     #roślina znika
+    if apple1.colliderect(goat): #zjadanie roślin przez kozy
+        apple.kolizja()     #roślina znika
 
+    koza.ruch()
+
+    odleglosc0 = odl(koza.poz_x, apple.poz_x, koza.poz_y, apple.poz_y) # porównuje odległość starej pozycji kozy z pozycją jabłka
+    odleglosc1 = odl(koza.poz_x1, apple.poz_x, koza.poz_y1, apple.poz_y) # porównuje odległość nowej pozycji kozy z pozycją jabłka
+    print("odl 0 =", odleglosc0, "odl 1 =", odleglosc1)
+    if odleglosc1<odleglosc0: #jeśli nowa odległość jest jest mniejsza niż stara, to koza się przesówa
+        koza.poz_x = koza.poz_x1
+        koza.poz_y = koza.poz_y1
+
+    #koza.ruch()
     pygame.display.update()
     pygame.display.flip()
-    clock.tick(0.75)
+    clock.tick(2)
 
